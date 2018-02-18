db69fc8b9b7b16d527bc5d6b9bbf14b771e01bed
Jeamay Palo <jeamay.palo@gmail.com>
2018-02-18 18:20:31 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -8,8 +8,10 @@ class School
   def add_student(name, grade)
     @student_name = name
     @grade = grade
-    if @roster.include(grade) == false
-    @roster[grade] = []
-  end
+      if @roster.include(grade) == false
+        @roster[grade] = []
+      end
     @roster[grade] << name
+  end
+
 end
