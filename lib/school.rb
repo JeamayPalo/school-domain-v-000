aa8b114709a349ebd12b6b8e43dad57cb3ffb31a
Jeamay Palo <jeamay.palo@gmail.com>
2018-02-18 18:22:17 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,8 +1,13 @@
 # code here!
 class School
+
   def initialize(school_name)
     @school_name = school_name
     @roster = {}
+  end
+
+  def roster
+    @roster
   end
 
   def add_student(name, grade)
